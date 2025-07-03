7f77caac2a644c7c3a5446e3aa9223419b186721
Tylor <tylor.9.kid@gmail.com>
2025-07-03 23:59:02 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+module.exports = {
+  //Enter your session id here (optional)
+  SESSION_ID: process.env.SESSION_ID || '',
+  
+  //Enter your number here for administrative access to the bot
+  BOT_ADMIN: process.env.BOT_ADMIN || '254712345678',
+  
+  //Bot web server port
+  PORT: parseInt(process.env.PORT) || 2605,
+  
+  //Enter your Github username here (Compulsory unless you have a valid premium key)
+  GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
+  
+  //Enter your desired bot password here. Users will be asked for this password when they try to connect to your bot via telegram or web
+  //Must be numbers only and a total of 8 digits
+  BOT_PASSWORD: parseInt(process.env.BOT_PASSWORD) || 12345678,
+  
+  //Enter the desired password for accessing administrative access to the bot 
+  //⚠️ Do not share with anyone as they can use it to control your bot
+   //Must be numbers only and a total of 8 digits
+  ADMIN_PASSWORD: parseInt(process.env.BOT_PASSWORD) || 11223344,
+  
+  //Enter telegram bot token for interaction with this bot via telegram (optional)
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
+  
+   //Antidelete and Antiedit functionality, set true to enable and false to disable. (⚠️ This function consumes a lot of memory + storage, only enable if you have enough resources ⚠️)
+   ANTI_DELETE: process.env.ANTI_DELETE || 'false',
+  
+  
+  //⚠️ Premium users settings ⚠️
+   PREMIUM_KEY: process.env.PREMIUM_KEY || '',
+   MAX_SESSIONS: parseInt(process.env.MAX_SESSIONS) || 3,
+   EXPIRY: process.env.EXPIRY || ''
+};
+
+// You must set GITHUB_USERNAME to use the bot unless you are a premium user
+// You must fork SPACE-MD repo in order to use the bot unless you're a premium user
